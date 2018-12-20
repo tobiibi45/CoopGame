b4776552b9bad6eadbf903d41e2673703d6b765a
Tobz <katafe1405@gmail.com>
2018-12-20 18:28:15 +0100

    Initial commit

---
+++ Source/CoopGame/Public/SCharacter.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class COOPGAME_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Moves character forward
+	void MoveForward(float Value);
+
+	//Moves character Backwards
+	void MoveRight(float Value);
+
+	//Allows the character to crouch
+	void BeginCrouch();
+
+	//Stops the character from crouching
+	void EndCrouch();
+
+	//Ataches a camera to the player for the third person controller mode
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USpringArmComponent* SpringArmComp;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Called to get the eye location for the character
+	virtual FVector GetPawnViewLocation() const override;
+
+};
